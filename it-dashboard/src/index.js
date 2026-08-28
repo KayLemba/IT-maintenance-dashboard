639bb8b052d544384ff82884ec24a19cd20d4e11
Kalolo Chola Lemba <kaylemba@yahoo.com>
2026-08-28 17:43:26 +0200

    Add new feature

--- it-dashboard/src/index.js
+++ it-dashboard/src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createRoot } from "react-dom/client";
 import "./styles.css";
+import "./accessibility.css";
+import "./evidence.css";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
